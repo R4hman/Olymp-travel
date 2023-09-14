575eb750e371eb0ca694b23b5ef6ba33aab3d9f8
R4hman <rehmanmysayev@gmail.com>
2023-09-14 18:41:48 +0400

    test h1 sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,7 +2,11 @@ import { Box } from "@mui/material";
 import React from "react";
 
 const SideBar = () => {
-  return <Box>sidebar</Box>;
+  return (
+    <Box>
+      <h1>Sidebar</h1>
+    </Box>
+  );
 };
 
 export default SideBar;
