a2940ae608e0f1996b92dedec70699c70d3bd91c
R4hman <rehmanmysayev@gmail.com>
2023-09-17 23:46:46 +0400

    changes in hortel custom hook

--- src/features/hotels/useHotels.js
+++ src/features/hotels/useHotels.js
@@ -2,14 +2,16 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchHotelsApi } from "../../services/apiHotels";
 
 const useHotels = () => {
-  const { isLoading: isHotelsLoading, data: hotels } = useQuery(
-    ["hotels"],
-    fetchHotelsApi
-  );
+  const {
+    isLoading: isHotelsLoading,
+    data: hotels,
+    error: errorHotels,
+  } = useQuery(["hotels"], fetchHotelsApi);
 
   return {
     isHotelsLoading,
     hotels,
+    errorHotels,
   };
 };
 
